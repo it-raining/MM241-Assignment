2d7e5bda02a6de8e6db609342df77fd78ca8dde6
KhoaHo <86819208+it-raining@users.noreply.github.com>
2024-12-15 23:11:27 +0700

    conflict resolve

--- policy.py
+++ policy.py
@@ -121,4 +121,5 @@ class GreedyPolicy(Policy):
 
                 if pos_x is not None and pos_y is not None:
                     break
+                
         return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
